61e4f55b256d016254f9633b3cedd33aba7fee3e
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-10-31 03:00:21 +0800

    Added vercel analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import './globals.css';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -39,6 +41,7 @@ export default function RootLayout({
 				<Header />
 				<main>{children}</main>
 				<Footer />
+				<Analytics />
 			</body>
 		</html>
 	);
