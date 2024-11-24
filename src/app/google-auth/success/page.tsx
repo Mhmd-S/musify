3c8e0d77bdcc3a77ca4804c9e8bb520a81817a31
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-11-24 13:29:21 +0800

    Fix

--- src/app/google-auth/success/page.tsx
+++ src/app/google-auth/success/page.tsx
@@ -2,22 +2,16 @@
 
 import { useEffect } from 'react';
 import { useAuth } from '@contexts/auth-context';
-import { useRouter } from 'next/navigation';
 import Spinner from '@components/Spinner';
 
 export default function GoogleAuthSuccess() {
+	const { isLoading, user } = useAuth();
 
-	const { checkAuth, user } = useAuth();
-	
 	useEffect(() => {
-		
-		checkAuth();
-		if (user) {
-			console.log('User is authenticated:', user);
-			console.log('User is authenticated:', window);
-			window.close()
+		if (user && !isLoading) {
+			window.close();
 		}
-	}, [user]);
+	}, [user, isLoading]);
 
 	return (
 		<div className="flex h-screen items-center justify-center">
